a020e88810b44b00d2f8ea494b4ee35c148df6c5
Dan Abrey <danabrey.com>
2020-07-28 08:35:19 +0100

    Bug: some Fleaflicker positions don't have a 'group', make nullable

--- src/Models/FleaflickerLeagueRosterPosition.php
+++ src/Models/FleaflickerLeagueRosterPosition.php
@@ -4,7 +4,7 @@ namespace DanAbrey\FleaflickerApi\Models;
 final class FleaflickerLeagueRosterPosition
 {
     public string $label;
-    public string $group;
+    public ?string $group = null;
     public array $eligibility = [];
     public ?int $min = null;
     public ?int $max = null;
